843f67259c0dbb9b5265bdc04b5f54c2438cc498
Manoj Santra <santramanoj1997@gmail.com>
2023-12-16 09:05:24 +0530

    shadcn library integrated successfully

---
+++ app/components/SideNav.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+
+interface SideNavProps {}
+const SideNav: FC<SideNavProps> = (): JSX.Element => {
+  return <div>SideNav</div>;
+};
+
+export default SideNav;
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactNode } from "react";
+import SideNav from "../components/SideNav";
+
+interface DashboardLayoutProps {
+  children: ReactNode;
+}
+
+const DashboardLayout: FC<DashboardLayoutProps> = ({
+  children,
+}): JSX.Element => {
+  return (
+    <div className="flex h-screen flex-col md:flex-row overflow-x-hidden overflow-y-auto ">
+      <div className="w-20 flex-none lg:w-64 md:border-r">
+        <SideNav />
+      </div>
+      <div className="flex-growmt-12 md:mt-0 flex-1 w-full md:overflow-y-auto overflow-x-hidden sm:p-6 md:p-12 max-w-7xl mx-auto ">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
